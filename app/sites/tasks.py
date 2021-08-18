909492f20c108a352b95b2ea9468a3d5d3406418
Kai Sawamoto <kaisawamoto@gmail.com>
2021-08-18 13:45:41 -0700

    train all devices

--- app/sites/tasks.py
+++ app/sites/tasks.py
@@ -5,37 +5,27 @@ from celery import shared_task
 from celery.signals import task_success
 import numpy as np
 from .DQN.TrainDRLGYM import TrainDRLGYM
-from core.models import TrainingResult
+from core.models import TrainingResult, Device
 from django.utils.timezone import now
 import datetime
 
 
-
 @shared_task
-def add(x, y):
-    return x + y
+def trainDRLGYM(FromHour, ToHour, W, Desire):
+    for device in Device.objects.all():
+        obj, created = TrainingResult.objects.get_or_create(device=device)
 
+        dqn, model_json = TrainDRLGYM(FromHour, ToHour, W, Desire)
+        weights_file = 'weights.h5'
+        dqn.save_weights(weights_file, overwrite=True)
+        Bytes = b''
+        with open(weights_file, "rb") as f:
+            while (byte := f.read(1)):
+                Bytes += byte
+        obj.weights_bin = Bytes
+        obj.model = model_json
+        obj.last_updated_at = datetime.datetime.now()
+        obj.save()
 
-@shared_task
-def trainDRLGYM(FromHour, ToHour, W, Desire, device_id=None):
-    res, created = TrainingResult.objects.get_or_create(
-        device_id=device_id,
-    )
-    if created:
-        res.save()
-    dqn, model_json = TrainDRLGYM(FromHour, ToHour, W, Desire)
-    weights_file = 'weights.h5'
-    dqn.save_weights(weights_file, overwrite=True)
-    Bytes = b''
-    with open(weights_file, "rb") as f:
-        while (byte := f.read(1)):
-            Bytes += byte
-    obj = TrainingResult.objects.get(device_id=device_id)
-    print(obj)
-    obj.weights_bin = Bytes
-    obj.model = model_json
-    obj.last_updated_at = datetime.datetime.now()
-    obj.save()
-
-    os.remove(weights_file)
-    assert not os.path.exists(weights_file)
+        os.remove(weights_file)
+        assert not os.path.exists(weights_file)
